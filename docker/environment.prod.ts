749e65f1efecc431cbe5f65c9f23c8ed1eabc73b
Marco Enrico Piras <kikkomep@crs4.it>
2021-05-19 13:19:22 +0000

    Add production environment to the Docker context

---
+++ docker/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  // 'configFile' is the path of a JSON resource containing
+  // the configuration properties that should be loaded
+  // and set without rebuild (e.g., apiBaseUrl, clientId).
+  // They overwrite the default environment settings defined at build-time
+  // and are exposed by the `AppConfigService.getConfig()` method.
+  configFile: "/assets/config.json",
+};
